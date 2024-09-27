af665453cd82a9af033e8fada5a5a14edd1fc570
udaykiran75 <s.udayakiran7550@gmail.com>
2024-09-27 07:40:40 +0000

    push

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,46 +1,82 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 
+import {v4 as uuidv4} from 'uuid'
 import {Component} from 'react'
+import AddtodoItem from '../TodoItem'
 
 const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    isChecked: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    isChecked: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    isChecked: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    isChecked: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    isChecked: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    isChecked: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    isChecked: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    isChecked: false,
   },
 ]
 
-import AddtodoItem from '../TodoItem'
-
 class SimpleTodos extends Component {
-  state = {todoList: initialTodosList}
+  state = {todoList: initialTodosList, inputTitle: '', showErrorMsg: false}
+
+  onAddNewTodoItem = () => {
+    const {inputTitle} = this.state
+
+    if (inputTitle !== '') {
+      const newTodo = {
+        id: uuidv4(),
+        title: inputTitle,
+        isChecked: false,
+      }
+      this.setState(prevState => ({
+        todoList: [...prevState.todoList, newTodo],
+        inputTitle: '',
+        showErrorMsg: false,
+      }))
+    } else {
+      this.setState({showErrorMsg: true})
+    }
+  }
+
+  onUpdateTodoItem = (todoId, title) => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.map(item =>
+        item.id === todoId ? {...item, title} : item,
+      ),
+    }))
+  }
 
   deleteItem = id => {
     const {todoList} = this.state
@@ -48,18 +84,51 @@ class SimpleTodos extends Component {
     this.setState({todoList: filteredItems})
   }
 
+  onChangeInputTitle = event => {
+    this.setState({inputTitle: event.target.value})
+  }
+
+  onChangeChecked = todoId => {
+    this.setState(prevState => ({
+      todoList: prevState.todoList.map(item =>
+        item.id === todoId ? {...item, isChecked: !item.isChecked} : item,
+      ),
+    }))
+  }
+
   render() {
-    const {todoList} = this.state
+    const {todoList, inputTitle, showErrorMsg} = this.state
     return (
       <div className="appcontainer">
         <div className="cardcontainer">
           <h1 className="heading">Simple Todos</h1>
+          <div className="mt-3 text-center">
+            <input
+              type="text"
+              value={inputTitle}
+              placeholder="Enter todo title"
+              onChange={this.onChangeInputTitle}
+              className="userInput-field"
+            />
+            <button
+              className="btn btn-primary add-button"
+              onClick={this.onAddNewTodoItem}
+              type="button"
+            >
+              Add
+            </button>
+            {showErrorMsg && (
+              <p className="text-danger">Please enter todo title</p>
+            )}
+          </div>
           <ul className="listItems">
             {todoList.map(eachItem => (
               <AddtodoItem
-                deleteItem={this.deleteItem}
-                todoItem={eachItem}
                 key={eachItem.id}
+                deleteItem={this.deleteItem}
+                onChangeChecked={this.onChangeChecked}
+                onUpdateTodoItem={this.onUpdateTodoItem}
+                todoItem={eachItem}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,14 +1,63 @@
 import './index.css'
+import {useState} from 'react'
+
 const AddtodoItem = props => {
-  const {todoItem, deleteItem} = props
-  const {id, title} = todoItem
+  const [inputField, setInputField] = useState('')
+  const [isEditing, setEdited] = useState(false)
+
+  const {todoItem, deleteItem, onChangeChecked, onUpdateTodoItem} = props
+  const {id, title, isChecked} = todoItem
+
+  const onClickEditBtn = () => {
+    setInputField(title)
+    setEdited(true)
+  }
+
+  const onClickSaveBtn = () => {
+    onUpdateTodoItem(id, inputField)
+    setEdited(false)
+  }
+
+  const onUpdateInput = event => {
+    setInputField(event.target.value)
+  }
+
   const onDelete = () => {
     deleteItem(id)
   }
-  return (
+
+  const onClickChecbox = () => {
+    onChangeChecked(id)
+  }
+
+  const textDeco = isChecked ? 'line-through' : ''
+
+  return isEditing ? (
+    <div className="mt-3 mb-3 edit-container">
+      <input
+        type="text"
+        value={inputField}
+        onChange={onUpdateInput}
+        className="edit-inputfield"
+      />
+      <button className="save-btn" onClick={onClickSaveBtn} type="button">
+        Save
+      </button>
+    </div>
+  ) : (
     <li>
-      <p>{title}</p>
-      <button onClick={onDelete}>Delete</button>
+      <div className="my-div">
+        <input type="checkbox" checked={isChecked} onChange={onClickChecbox} />
+        <p className={`title ${textDeco}`}>{title}</p>
+      </div>
+      <div className="d-flex">
+        <button onClick={onClickEditBtn} className="edit-button" type="button">
+          Edit
+        </button>
+        <button onClick={onDelete} type="button">
+          Delete
+        </button>
+      </div>
     </li>
   )
 }
